65ea4b6b6134d35fe57d6c689a9e8e42c50b4528
shopnilashik <shopnil.ashik@gmail.com>
2022-08-28 13:57:24 +0600

    Note section

---
+++ webapp/controller/Footer.controller.js
@@ -0,0 +1,30 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel) {
+    "use strict";
+
+    return Controller.extend("OrderSystem.ordersystem.controller.Footer", {
+      onInit: function () {
+        var oJsonModel = new JSONModel({
+          Note: [
+            {
+              type: "NP01 - Header Note",
+              text: "This is a note",
+              delete: "x",
+            },
+            {
+              type: "NP02 - Generic Note",
+              text: "This is a generic note",
+              delete: "x",
+            },
+          ],
+        });
+        this.getView().setModel(oJsonModel, "oOrderData");
+
+      },
+    });
+  }
+);
